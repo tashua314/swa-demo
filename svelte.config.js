5ddc5c8c5c01337dac2d6b94e51567fbdb128f6f
Yuta Takahashi <m1205123ing@gmail.com>
2022-07-26 18:40:20 +0900

    configure

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
+import adapter from '@sveltejs/adapter-node';
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,11 +10,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: true
-    }),
+		adapter: adapter(),
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
